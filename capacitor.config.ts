57312469d8266147cf62f5329ddc0afc87407d0b
jonatxul2014 <elchinoxu@gmail.com>
2024-11-26 19:46:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LeccionCrearComponentes',
+  webDir: 'www'
+};
+
+export default config;
